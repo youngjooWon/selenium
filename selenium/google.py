e1c8cc684a407664af84bf18fd63590991149791
youngjooWon <wyj312@gmail.com>
2021-01-17 22:10:44 +0900

    google image download

---
+++ selenium/google.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("푸바오")
+elem.send_keys(Keys.RETURN)
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for image in images
+    try:
+        image.click()
+        time.sleep(3)
+        imgURL = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
+        urllib.request.urlretrieve(imgURL, str(count) + ".jpg")
+        count = count + 1
+    except: pass  
+ driver.close()
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
